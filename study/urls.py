bd7bbdbec18976c4505e075eee173e9eb2f32dcb
Baltoncome <Baltoncome@gmail.com>
2022-09-27 16:04:32 +0200

    Views and Urls

--- study/urls.py
+++ study/urls.py
@@ -1,16 +1,8 @@
-from django.http import HttpResponse
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-
-def home(request):
-    return HttpResponse('Home Page')
-
-def room(request):
-    return HttpResponse('Rooms')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
+    path('', include('base.urls'))
 ]
