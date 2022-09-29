d14259d25cc62d5d3246743cc0f29648c7eb02cf
Baltoncome <Baltoncome@gmail.com>
2022-09-29 12:05:20 +0200

    Database & Admin Panel

--- base/views.py
+++ base/views.py
@@ -1,10 +1,24 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
 
+
+'''rooms = [
+    { 'id': 1, 'name': 'Pythoners' },
+    { 'id': 2, 'name': 'Javanators' },
+    { 'id': 3, 'name': 'PHPillers' },
+    { 'id': 4, 'name': 'Basers' },
+    { 'id': 5, 'name': 'Laracaster' },
+]'''
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home Page')
+    rooms = Room.objects.all()
+    context = { 'rooms' : rooms }
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('Rooms')
+
+def room(request, key):
+    room = Room.objects.get(id=key)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
