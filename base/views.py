c94286b503b85c57433177dce4cd560eae5eb82a
Baltoncome <Baltoncome@gmail.com>
2022-10-07 09:55:59 +0200

    User login & logout

--- base/views.py
+++ base/views.py
@@ -1,11 +1,39 @@
 from multiprocessing import context
+from pickle import NONE
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except: 
+            messages.error(request, 'The user does not exist')
+        user = authenticate(request, username=username, password=password)
+        if user is not None: 
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Bad credentials')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
@@ -13,8 +41,9 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context = { 'rooms' : rooms , 'topics' : topics}
+    context = { 'rooms' : rooms , 'topics' : topics, 'room_count' : room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, key):
