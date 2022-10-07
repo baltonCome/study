57e0ad72ddd5ffafa0cf4850d675c4d58083fcf5
Baltoncome <Baltoncome@gmail.com>
2022-10-07 16:28:08 +0200

    activity feed

--- base/urls.py
+++ base/urls.py
@@ -5,10 +5,15 @@ urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
+    path('profile/<str:key>', views.userProfile, name="user-profile"),
+
     path('', views.home, name="home"),
+
     path('room/<str:key>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:key>', views.updateRoom, name="update-room"),
     path('delete-room/<str:key>', views.deleteRoom, name="delete-room"),
+
     path('delete-message/<str:key>', views.deleteMessage, name="delete-message"),
+
 ]
--- base/views.py
+++ base/views.py
@@ -65,15 +65,19 @@ def home(request):
     )
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = { 'rooms' : rooms , 'topics' : topics, 'room_count' : room_count}
+    room_messages = Message.objects.filter(
+    Q(room__topic__name__icontains=q)
+    )
+    context = { 'rooms' : rooms , 'topics' : topics, 'room_count' : room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 
 def room(request, key):
     room = Room.objects.get(id=key)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
+
     if request.method == 'POST':
         message = Message.objects.create(
             user = request.user,
@@ -84,6 +88,15 @@ def room(request, key):
         return redirect('room', key = room.id)
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
+
+
+def userProfile(request, key):
+    user = User.objects.get(id=key)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user' : user , 'rooms' : rooms, 'room_messages': room_messages, 'topics' : topics }
+    return render(request, 'base/profile.html', context)
 
 
 
