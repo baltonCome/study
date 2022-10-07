3320cde4e5bca67a2a1b8b30b6dd04ba55ee0cca
Baltoncome <Baltoncome@gmail.com>
2022-10-07 10:56:04 +0200

    User registration

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
@@ -7,14 +8,16 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -27,14 +30,30 @@ def loginPage(request):
         else:
             messages.error(request, 'Bad credentials')
 
-    context = {}
+    context = { 'page' : page}
     return render(request, 'base/login_register.html', context)
-
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            return redirect('home')
+            login(request, user)
+        else:
+            messages.error(request, 'Error')
+    return render(request, 'base/login_register.html', {'form' : form})
+
 
 
 def home(request):
